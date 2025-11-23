21dc4b8a8ba1fc60acf87186e1132e07b4d98529
PutuRifkiDy <puturifki56@gmail.com>
2025-11-23 23:42:39 +0800

    added: setup expressjs, psql, nodemon, nodepgmigrate, dotenv

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const Pool = require('pg').Pool;
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// list end points
+app.get('/', (request, response) => {
+  response.json({ Welcome: 'Testing' })
+});
+// contoh
+// app.get('/countries', db.getCountries);
+
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
+
