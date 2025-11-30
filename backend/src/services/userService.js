8f0bf9778792ea6714f46ec4d8551c2364f4f3aa
PutuRifkiDy <puturifki56@gmail.com>
2025-11-30 16:39:47 +0800

    added: updateUser and deleteUser function in services

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -30,6 +30,22 @@ class UserService {
         const { password, ...userWithoutPassword } = user;
         return userWithoutPassword;
     }
+
+    async updateUserId(id, { name, email, password, role }) {
+        const result = await db.query('UPDATE users SET name = $1, email = $2, role = $3, password = $4 WHERE id = $5 RETURNING id', [name, email, role, password, id]);
+
+        if (!result.rows.length) {
+            throw new Error('User not found');
+        }
+    }
+
+    async deleteUserById(id) {
+        const result = await db.query('DELETE FROM users WHERE id = $1 RETURNING id', [id]);
+        if (!result.rows.length) {
+            throw new Error('User not found');
+        }
+    }
+
 }
 
 module.exports = new UserService();
