7f4b174168b74a23cbb9695b960cc74a8b07c5c4
PutuRifkiDy <puturifki56@gmail.com>
2025-12-02 21:45:09 +0800

    added: call api add, call api edit, call api delete.

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,16 +1,15 @@
 const BASE_URL = "http://localhost:3000";
 
-
 function getAccessToken() {
-  return localStorage.getItem('accessToken');
+  return localStorage.getItem("accessToken");
 }
 
 function putAccessToken(accessToken) {
-  return localStorage.setItem('accessToken', accessToken)
+  return localStorage.setItem("accessToken", accessToken);
 }
 
 function removeAccessToken() {
-  return localStorage.removeItem('accessToken')
+  return localStorage.removeItem("accessToken");
 }
 
 async function fetchWithToken(url, options = {}) {
@@ -18,92 +17,144 @@ async function fetchWithToken(url, options = {}) {
     ...options,
     headers: {
       ...options.headers,
-      Authorization: `Bearer ${getAccessToken()}`
-    }
-  })
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
 }
 
 async function login({ email, password }) {
   try {
     const response = await fetch(`${BASE_URL}/auth/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password }),
     });
 
     const responseJson = await response.json();
-    console.log('Response:', responseJson); // Debug log
+    console.log("Response:", responseJson); // Debug log
 
-    if (responseJson.status !== 'success') {
+    if (responseJson.status !== "success") {
       return {
         error: true,
-        message: responseJson.message || 'Login gagal'
-      }
+        message: responseJson.message || "Login gagal",
+      };
     }
 
     if (!responseJson.accessToken) {
       return {
         error: true,
-        message: 'Response data tidak valid'
-      }
+        message: "Response data tidak valid",
+      };
     }
 
     putAccessToken(responseJson.accessToken);
     return {
       error: false,
-      data: responseJson
-    }
+      data: responseJson,
+    };
   } catch (error) {
     return {
       error: true,
-      message: error
-    }
+      message: error,
+    };
   }
 }
 
 async function logout(refreshToken) {
   try {
     await fetch(`${BASE_URL}/auth/logout`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ refreshToken })
+      body: JSON.stringify({ refreshToken }),
     });
 
     removeAccessToken();
-    return { error: false }
+    return { error: false };
   } catch (error) {
     console.error(error);
     removeAccessToken();
-    return { error: false }
+    return { error: false };
   }
 }
 
 async function deleteUser(id) {
   try {
     const response = await fetchWithToken(`${BASE_URL}/users/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      },
+      method: "DELETE",
     });
 
     const responseJson = await response.json();
 
-    if (responseJson.status !== 'success') {
+    if (responseJson.status !== "success") {
       return {
         error: true,
-        message: responseJson.message || 'Gagal menghapus user'
-      }
+        data: null,
+      };
     }
-  } catch (error) {
     return {
-      error: true,
-      message: error
+      error: false,
+      data: responseJson,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addUser({ name, email, password, role }) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password, role }),
+    });
+
+    const responseJson = await response.json();
+
+    if (responseJson.status !== "success") {
+      return {
+        error: true,
+        data: null,
+      };
     }
+    return {
+      error: false,
+      data: responseJson.data,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function editUser(id, { name, email, password, role }) {
+  try {
+    const response = await fetchWithToken(`${BASE_URL}/users/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password, role }),
+    });
+
+    const responseJson = await response.json();
+
+    if (responseJson.status !== "success") {
+      return {
+        error: true,
+        data: null,
+      };
+    }
+    return {
+      error: false,
+      data: responseJson.data,
+    };
+  } catch (error) {
+    console.error(error);
   }
 }
 
@@ -114,5 +165,7 @@ export {
   login,
   logout,
   fetchWithToken,
+  addUser,
   deleteUser,
-}
+  editUser,
+};
