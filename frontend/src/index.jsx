c8c997ebc7f355d101da98b1015adbe06817319d
PutuRifkiDy <puturifki56@gmail.com>
2025-12-02 23:36:48 +0800

    added: toaster shaCDNUI

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,45 +1,46 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import Login from './pages/auth/Login';
-import Dashboard from './pages/app/Dashboard';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/app/Dashboard";
 
-import './index.css';
-import { ThemeProvider } from './components/ThemeProvider';
-import Create from './pages/app/users/Create';
-import Update from './pages/app/users/Update';
+import "./index.css";
+import { ThemeProvider } from "./components/ThemeProvider";
+import Create from "./pages/app/users/Create";
+import Update from "./pages/app/users/Update";
+import { Toaster } from "./components/ui/sonner";
 
 const router = createBrowserRouter([
   {
-    path: '/dashboard',
-    element: <Dashboard />
+    path: "/dashboard",
+    element: <Dashboard />,
   },
   {
-    path: '/admin/users',
-    element: <Dashboard />
+    path: "/admin/users",
+    element: <Dashboard />,
   },
   {
-    path: '/admin/users/create',
-    element: <Create />
+    path: "/admin/users/create",
+    element: <Create />,
   },
   {
-    path: '/admin/users/update',
-    element: <Update />
+    path: "/admin/users/update",
+    element: <Update />,
   },
   {
-    path: '/chatbot',
-    element: <Dashboard />
+    path: "/chatbot",
+    element: <Dashboard />,
   },
   {
-    path: '/login',
-    element: <Login />
-  }
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider defaultTheme='light' storageKey='theme'>
+  <ThemeProvider defaultTheme="light" storageKey="theme">
+    <Toaster richColors position="top-center" />
     <RouterProvider router={router} />
   </ThemeProvider>
 );
-
