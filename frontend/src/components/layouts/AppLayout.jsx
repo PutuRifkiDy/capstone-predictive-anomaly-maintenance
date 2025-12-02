c876a9b695c521adf47b48767cadb9e5dd1aefe1
PutuRifkiDy <puturifki56@gmail.com>
2025-12-02 16:00:32 +0800

    added: Applayout, sidebar, sidebar responsive

--- frontend/src/components/layouts/AppLayout.jsx
+++ frontend/src/components/layouts/AppLayout.jsx
@@ -1,21 +1,150 @@
+import { ViewColumnsIcon, WindowIcon } from "@heroicons/react/24/outline";
 import CompanyLogo from "../icons/CompanyLogo";
 import Sidebar from "./partials/Sidebar";
+import { Fragment, useState } from "react";
+import { ThemeSwitcher } from "../ThemeSwitcher";
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import SidebarResponsive from "./partials/SidebarResponsive";
 
 export default function AppLayout({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpenMobile, setIsSidebarOpenMobile] = useState(false);
+
+  const handleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
   return (
     <>
       <div>
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
-            <header className="flex items-center gap-3 shrink-0 h-24">
-              <CompanyLogo />
-              <p className="font-bold text-[22px] leading-[150%] tracking-[-1%]">Predicta<span className="text-[#515DEF]">Energy</span></p>
-            </header>
-            <Sidebar />
+        {/* sidebar mobile */}
+        <Transition.Root show={isSidebarOpenMobile} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-50 lg:hidden"
+            onClose={setIsSidebarOpenMobile}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-900/80" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                      <button
+                        type="button"
+                        className="-m-2.5 p-2.5"
+                        onClick={() => setIsSidebarOpenMobile(false)}
+                      >
+                        <XMarkIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  {/* Sidebar mobile */}
+                  <SidebarResponsive
+                    isSidebarOpenMobile={isSidebarOpenMobile}
+                  />
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* sidebar dekstop */}
+        <div
+          className={`hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:flex-col transition-all duration-300 ease-in-out 
+          ${isSidebarOpen == true ? "lg:w-72" : "lg:w-24"}`}
+        >
+          <div
+            className={`flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 dark:border-gray-500
+            ${isSidebarOpen == true ? "px-6" : "px-0"}`}
+          >
+            {isSidebarOpen == true ? (
+              <header className="flex items-center gap-3 shrink-0 h-24">
+                <CompanyLogo />
+                <p className="font-bold text-[22px] leading-[150%] tracking-[-1%]">
+                  Predicta
+                  <span className="text-[#515DEF]">Energy</span>
+                </p>
+              </header>
+            ) : (
+              <header className="flex items-center justify-center gap-3 shrink-0 h-24">
+                <CompanyLogo />
+              </header>
+            )}
+            <Sidebar isSidebarOpen={isSidebarOpen} />
           </div>
         </div>
-        <main className="py-10 lg:pl-72 bg-gray-50">
-          <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+
+        {/* header mobile */}
+        <div className="sticky top-0 z-40 flex items-center gap-x-6 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            onClick={() => setIsSidebarOpenMobile(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <ViewColumnsIcon className="h-6 w-6 text-foreground" />
+          </button>
+          <div className="h-5 w-0.5 bg-gray-300" />
+          <ThemeSwitcher />
+        </div>
+
+        <main
+          className={`${
+            isSidebarOpen == true ? "lg:pl-72" : "lg:pl-24"
+          } transition-all duration-300 ease-in-out relative`}
+        >
+          {/* header dekstop */}
+          <div className="py-5 lg:flex hidden items-center justify-between">
+            <div className="flex items-center gap-3">
+              <button className="border-0 hover:bg-gray-200 dark:hover:bg-[#7E89AC]/20 p-2 sm:ml-2 lg:ml-4 w-fit rounded-lg transition-all duration-300 ease-in-out group">
+                <ViewColumnsIcon
+                  className="w-6 h-6 text-gray-600"
+                  onClick={handleSidebar}
+                />
+              </button>
+              <div className="h-5 w-0.5 bg-gray-300" />
+              <p className="text-[16px] tracking-[-0.25px] text-gray-500">
+                <span className="font-medium">
+                  Predicta<span className="text-[#515DEF]">Energy</span>
+                </span>{" "}
+                aplikasi web untuk cek mesin anda.
+              </p>
+            </div>
+            <div className="pr-5">
+              <ThemeSwitcher />
+            </div>
+          </div>
+          <div className="px-4 sm:px-6 lg:px-8 mt-5">{children}</div>
+          <div className="absolute bottom-0 right-5"></div>
         </main>
       </div>
     </>
--- frontend/src/components/layouts/partials/SidebarResponsive.jsx
+++ frontend/src/components/layouts/partials/SidebarResponsive.jsx
@@ -1,7 +1,247 @@
-export default function SidebarResponsive() {
-  return(
-    <>
-    
-    </>
+import CompanyLogo from "@/components/icons/CompanyLogo";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  HomeIcon,
+  UserGroupIcon,
+  ChatBubbleLeftRightIcon,
+} from "@heroicons/react/24/outline";
+import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/solid";
+import { Link, useLocation } from "react-router";
+
+export default function SidebarResponsive({ isSidebarOpenMobile }) {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  return (
+    <nav className="flex flex-1 flex-col bg-white dark:bg-[#081028]">
+      <header className="flex justify-center items-center gap-3 shrink-0 h-24">
+        <CompanyLogo />
+        <p className="font-bold text-[22px] leading-[150%] tracking-[-1%]">
+          Predicta
+          <span className="text-[#515DEF]">Energy</span>
+        </p>
+      </header>
+      <ul className="flex flex-1 flex-col gap-y-7" role="list">
+        {isSidebarOpenMobile == true ? (
+          <>
+            <li>
+              <ul role="list" className="space-y-2">
+                <li>
+                  <Link
+                    to={"/dashboard"}
+                    className={`
+                  ${
+                    currentPath === "/dashboard"
+                      ? "bg-[#515DEF]/10 relative text-[#515DEF] pl-5"
+                      : "text-foreground hover:bg-gray-100 dark:hover:bg-[#7E89AC]/30 transition-all duration-300 ease-in-out"
+                  }
+                  group flex gap-x-3 rounded-md p-3 text-sm font-medium items-center
+                `}
+                  >
+                    {currentPath === "/dashboard" && (
+                      <div className="absolute left-0 inset-y-0 bg-[#515DEF] w-1 rounded-l-[10px]" />
+                    )}
+                    <HomeIcon
+                      className={`w-6 h-6 ${
+                        currentPath === "/dashboard"
+                          ? "text-[#515DEF]"
+                          : "text-foreground"
+                      }`}
+                    />
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/admin/users"}
+                    className={`
+                  ${
+                    currentPath === "/admin/users"
+                      ? "bg-[#515DEF]/10 relative text-[#515DEF] pl-5"
+                      : "text-foreground hover:bg-gray-100 dark:hover:bg-[#7E89AC]/30 transition-all duration-300 ease-in-out"
+                  }
+                  group flex gap-x-3 rounded-md p-3 text-sm font-medium items-center
+                `}
+                  >
+                    {currentPath === "/admin/users" && (
+                      <div className="absolute left-0 inset-y-0 bg-[#515DEF] w-1 rounded-l-[10px]" />
+                    )}
+                    <UserGroupIcon
+                      className={`w-6 h-6 ${
+                        currentPath === "/admin/users"
+                          ? "text-[#515DEF]"
+                          : "text-foreground"
+                      }`}
+                    />
+                    Manajemen Pengguna
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/chatbot"}
+                    className={`
+                  ${
+                    currentPath === "/chatbot"
+                      ? "bg-[#515DEF]/10 relative text-[#515DEF] pl-5"
+                      : "text-foreground hover:bg-gray-100 dark:hover:bg-[#7E89AC]/30 transition-all duration-300 ease-in-out"
+                  }
+                  group flex gap-x-3 rounded-md p-3 text-sm font-medium items-center
+                `}
+                  >
+                    {currentPath === "/chatbot" && (
+                      <div className="absolute left-0 inset-y-0 bg-[#515DEF] w-1 rounded-l-[10px]" />
+                    )}
+                    <ChatBubbleLeftRightIcon
+                      className={`w-6 h-6 ${
+                        currentPath === "/chatbot"
+                          ? "text-[#515DEF]"
+                          : "text-foreground"
+                      }`}
+                    />
+                    Chatbot
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="mt-auto flex items-center justify-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger className="py-6 flex gap-2 items-center">
+                  <img src="/profile-sidebar-image.png" alt="sidebar-profile" />
+                  <div className="flex flex-col gap-1 justify-center">
+                    <p className="font-medium text-[14px] tracking-[-0.25px] text-foreground">
+                      Putu Rifki Dirkayuda
+                    </p>
+                    <div className="flex justify-center font-medium items-center bg-[#515DEF] rounded-full py-1 px-3 text-white w-fit text-[10px]">
+                      Admin
+                    </div>
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="min-w-[13rem] flex flex-col gap-1 dark:bg-[#081028]">
+                  <DropdownMenuItem className="flex gap-5 items-center cursor-pointer font-medium">
+                    <HomeIcon className="!w-6 !h-6 font-medium flex-shrink-0" />
+                    Beranda
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="flex gap-5 items-center cursor-pointer text-red-600 font-medium">
+                    <ArrowLeftEndOnRectangleIcon className="!w-6 !h-6 font-bold text-red-600 flex-shrink-0" />
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <ul role="list" className="space-y-2">
+                <li>
+                  <Link
+                    to={"/dashboard"}
+                    className={`
+                  ${
+                    currentPath === "/dashboard"
+                      ? "bg-[#515DEF]/10 relative text-[#515DEF]"
+                      : "text-foreground hover:bg-gray-100 dark:hover:bg-[#7E89AC]/30 transition-all duration-300 ease-in-out"
+                  }
+                  group flex gap-x-3 ${
+                    isSidebarOpenMobile == true ? "rounded-md" : ""
+                  } p-3 text-sm font-medium items-center justify-center
+                `}
+                  >
+                    {currentPath === "/dashboard" && (
+                      <div className="absolute left-0 inset-y-0 bg-[#515DEF] w-1 rounded-l-[10px]" />
+                    )}
+                    <HomeIcon
+                      className={`w-6 h-6 ${
+                        currentPath === "/dashboard"
+                          ? "text-[#515DEF]"
+                          : "text-foreground"
+                      }`}
+                    />
+                    {/* Dashboard */}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/admin/users"}
+                    className={`
+                  ${
+                    currentPath === "/admin/users"
+                      ? "bg-[#515DEF]/10 relative text-[#515DEF]"
+                      : "text-foreground hover:bg-gray-100 dark:hover:bg-[#7E89AC]/30 transition-all duration-300 ease-in-out"
+                  }
+                  group flex gap-x-3 ${
+                    isSidebarOpenMobile == true ? "rounded-md" : ""
+                  } p-3 text-sm font-medium items-center justify-center
+                `}
+                  >
+                    {currentPath === "/admin/users" && (
+                      <div className="absolute left-0 inset-y-0 bg-[#515DEF] w-1 rounded-l-[10px]" />
+                    )}
+                    <UserGroupIcon
+                      className={`w-6 h-6 ${
+                        currentPath === "/admin/users"
+                          ? "text-[#515DEF]"
+                          : "text-foreground"
+                      }`}
+                    />
+                    {/* Manajemen Pengguna */}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/chatbot"}
+                    className={`
+                  ${
+                    currentPath === "/chatbot"
+                      ? "bg-[#515DEF]/10 relative text-[#515DEF]"
+                      : "text-foreground hover:bg-gray-100 dark:hover:bg-[#7E89AC]/30 transition-all duration-300 ease-in-out"
+                  }
+                  group flex gap-x-3 ${
+                    isSidebarOpenMobile == true ? "rounded-md" : ""
+                  } p-3 text-sm font-medium items-center justify-center
+                `}
+                  >
+                    {currentPath === "/chatbot" && (
+                      <div className="absolute left-0 inset-y-0 bg-[#515DEF] w-1 rounded-l-[10px]" />
+                    )}
+                    <ChatBubbleLeftRightIcon
+                      className={`w-6 h-6 ${
+                        currentPath === "/chatbot"
+                          ? "text-[#515DEF]"
+                          : "text-foreground"
+                      }`}
+                    />
+                    {/* Chatbot */}
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="mt-auto flex items-center justify-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger className="py-6 flex gap-2 items-center">
+                  <img src="/profile-sidebar-image.png" alt="sidebar-profile" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="min-w-[13rem] flex flex-col gap-1 dark:bg-[#081028]">
+                  <DropdownMenuItem className="flex gap-5 items-center cursor-pointer font-medium">
+                    <HomeIcon className="!w-6 !h-6 font-medium flex-shrink-0" />
+                    Beranda
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="flex gap-5 items-center cursor-pointer text-red-600 font-medium">
+                    <ArrowLeftEndOnRectangleIcon className="!w-6 !h-6 font-bold text-red-600 flex-shrink-0" />
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 }
