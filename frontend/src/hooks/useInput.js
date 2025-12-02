9d8854b42552a4ed6802b38098f349f74ca674ef
PutuRifkiDy <puturifki56@gmail.com>
2025-12-02 22:00:53 +0800

    added: hooks useInput

---
+++ frontend/src/hooks/useInput.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function useInput(defaultValue = "") {
+  const [value, setValue] = useState(defaultValue);
+
+  const onValueChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    value,
+    onValueChangeHandler,
+  };
+}
+
+export default useInput;
