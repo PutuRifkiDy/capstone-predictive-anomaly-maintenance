4ce5cc5937bff2a49877ba165e6d439dbebced42
tridarma6 <triidarma06@gmail.com>
2025-12-02 20:01:08 +0800

    add access & refresh token at env.example backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,3 +3,48 @@ const BASE_URL = "http://localhost:3000";
 // candra
 
 // tri
+
+function getAccessToken() {
+  return localStorage.getItem('accessToken');
+}
+
+function putAccessToken(accessToken) {
+  return localStorage.setItem('accessToken', accessToken)
+}
+
+async function fetchWithToken(url, options = {}) {
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${getAccessToken()}`
+    }
+  })
+}
+
+async function login({ email, password }) {
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  });
+
+  const responseJson = await response.json();
+  
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return {
+      error: true,
+      data: null
+    }
+  }
+}
+
+export {
+  getAccessToken,
+  putAccessToken,
+  login,
+  fetchWithToken
+}
