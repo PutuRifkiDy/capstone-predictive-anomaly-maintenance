8ae9337d3a33fb4811fd9464b01e09b7b10b6b5d
PutuRifkiDy <puturifki56@gmail.com>
2025-12-02 16:01:18 +0800

    added: darkmode using shaCDNUI

---
+++ frontend/src/components/ThemeSwitcher.jsx
@@ -0,0 +1,24 @@
+import { useTheme } from '@/Components/ThemeProvider';
+import { Button } from '@/Components/ui/button';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
+
+export function ThemeSwitcher() {
+	const { theme, setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+	return (
+		<Button onClick={toggleTheme} variant="none" className="rounded-full flex items-center w-12 h-12 dark:bg-[#515DEF]/10">
+			<div
+				className={`flex items-center gap-x-2 transition-transform duration-500`}
+			>
+				{theme === 'dark' ? (
+					<SunIcon className="!h-7 !w-7 text-yellow-400" />
+				) : (
+					<MoonIcon className="!h-7 !w-7 text-[#515DEF]" />
+				)}
+			</div>
+		</Button>
+	);
+}
