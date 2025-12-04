d78ff2cfb1cf7572f68ddfcb9ae1cffbbfef1b25
PutuRifkiDy <puturifki56@gmail.com>
2025-12-04 15:17:51 +0800

    updated datatable

--- frontend/src/pages/app/users/Index.jsx
+++ frontend/src/pages/app/users/Index.jsx
@@ -2,6 +2,13 @@ import AppLayout from "@/components/layouts/AppLayout";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
   Table,
   TableBody,
   TableCell,
@@ -9,7 +16,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { getUsers } from "@/utils/api";
+import { deleteUser, getUsers } from "@/utils/api";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 import {
   flexRender,
   getCoreRowModel,
@@ -19,12 +27,31 @@ import {
 } from "@tanstack/react-table";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
+import { toast } from "sonner";
 
 export default function Index({ authedUser, onLogout }) {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
 
-  // tanstack tablenya
+  const fetchUsersData = async () => {
+    const result = await getUsers();
+    if (!result.error) {
+      setUsers(result.data);
+    } else {
+      setUsers([]);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUsersData();
+  }, []);
+
+  // copyan tanstacktablenya
   const [columnFilters, setColumnFilters] = useState([]);
   const columns = [
     {
@@ -45,7 +72,9 @@ export default function Index({ authedUser, onLogout }) {
       accessorKey: "role",
       header: "Role",
       cell: ({ row }) => (
-        <div className="font-normal">{row.getValue("role")}</div>
+        <div className={`font-medium p-2 rounded-md w-fit ${row.getValue("role") == "admin" ? "bg-yellow-100 text-yellow-500 w-fit" : "bg-[#515DEF]/10 text-[#515DEF]"}`}>
+          {row.getValue("role") == "admin" ? "Admin" : "Engineer"}
+        </div>
       ),
     },
     {
@@ -57,31 +86,31 @@ export default function Index({ authedUser, onLogout }) {
           <div className="flex gap-2">
             <Link
               to={`/admin/users/update/${user.id}`}
-              className="text-sm text-blue-600 hover:underline"
+              className="text-sm bg-yellow-100 rounded-md p-2"
             >
-              Edit
+              <PencilSquareIcon className="h-4 w-4 text-yellow-500" />
             </Link>
-            {/* <Link to={`/admin/users/delete/${user.id}`} className="text-sm text-red-600 hover:underline">Edit</Link> */}
+            <Link
+              className="text-sm bg-red-100 rounded-md p-2"
+              onClick={async () => {
+                const response = await deleteUser(user.id);
+                if (response.error) {
+                  toast.error(response.message);
+                } else {
+                  fetchUsersData();
+                  toast.success(response.message);
+                }
+              }}
+            >
+              <TrashIcon className="h-4 w-4 text-red-500" />
+            </Link>
           </div>
         );
       },
     },
   ];
 
-  useEffect(() => {
-    const fetchUsersData = async () => {
-      const result = await getUsers();
-      if (!result.error) {
-        setUsers(result.data);
-      } else {
-        setUsers([]);
-      }
-      setLoading(false);
-    };
-    fetchUsersData();
-  }, []);
-
-  // tanstacktablenya
+  // copyan tanstacktablenya
   const table = useReactTable({
     data: users,
     columns,
@@ -89,9 +118,10 @@ export default function Index({ authedUser, onLogout }) {
     onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
-
+    onPaginationChange: setPagination,
     state: {
       columnFilters,
+      pagination,
     },
   });
 
@@ -173,11 +203,43 @@ export default function Index({ authedUser, onLogout }) {
               </Table>
             </div>
 
-            <div className="flex items-center justify-end space-x-2 py-4">
+            <div className="flex items-center justify-between space-x-2 py-4">
+              <div className="flex text-sm text-muted-foreground">
+                Page {table.getState().pagination.pageIndex + 1} of{" "}
+                {table.getPageCount()} (
+                {table.getFilteredRowModel().rows.length} rows total)
+              </div>
+
+              <div className="flex items-center space-x-6">
+                <div className="flex items-center gap-2">
+                  <p className="text-sm text-muted-foreground">Rows per page</p>
+                  <Select
+                    value={`${table.getState().pagination.pageSize}`}
+                    onValueChange={(value) => {
+                      table.setPageSize(Number(value));
+                    }}
+                  >
+                    <SelectTrigger className="h-8 w-[70px]">
+                      <SelectValue
+                        placeholder={table.getState().pagination.pageSize}
+                      />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {[10, 20, 30, 40, 50].map((pageSize) => (
+                        <SelectItem key={pageSize} value={`${pageSize}`}>
+                          {pageSize}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+
               <div className="space-x-2">
                 <Button
                   variant="outline"
                   size="sm"
+                  className="dark:bg-[#515DEF]/10"
                   onClick={() => table.previousPage()}
                   disabled={!table.getCanPreviousPage()}
                 >
@@ -186,6 +248,7 @@ export default function Index({ authedUser, onLogout }) {
                 <Button
                   variant="outline"
                   size="sm"
+                  className="dark:bg-[#515DEF]/10"
                   onClick={() => table.nextPage()}
                   disabled={!table.getCanNextPage()}
                 >
