188d7ae948b2b2bc49d786db844408e1636776c1
Candra Wikananta <candrawikananta@gmail.com>
2025-12-06 20:52:13 +0800

    update: responsive in dashboard

--- frontend/src/pages/app/Dashboard.jsx
+++ frontend/src/pages/app/Dashboard.jsx
@@ -10,58 +10,50 @@ export default function Dashboard({ authedUser, onLogout }) {
       <p className="font-medium text-[32px] text-[#000000] dark:text-white">
         Dashboard
       </p>
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-3 pt-[45px]">
-        <div className="flex flex-row justify-center items-center bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] gap-[35px] shadow-sm">
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 pt-[45px]">
+        <div className="flex items-center justify-center flex-row bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[20px] py-[20px]  gap-[20px] shadow-sm">
           <div>
             <p className="text-gray-400">Total Machine</p>
-            <p className="font-medium text-[34px]">
-              10.000
-            </p>
+            <p className="font-medium text-[34px]">10.000</p>
           </div>
           <div className="bg-[#515DEF]/30 rounded-[10px] h-[69px] w-[69px] flex justify-center items-center">
             <MachineIcon />
           </div>
         </div>
-        <div className="flex flex-row justify-center items-center bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] gap-[35px] shadow-sm">
+        <div className="flex flex-row justify-center items-center bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[20px] py-[20px] gap-[20px] shadow-sm">
           <div>
             <p className="text-gray-400">Normal Machine</p>
-            <p className="font-medium text-[34px]">
-              9.300
-            </p>
+            <p className="font-medium text-[34px]">9.300</p>
           </div>
           <div className="bg-[#4AD991]/30 rounded-[10px] h-[69px] w-[69px] flex justify-center items-center">
             <MachineNormalIcon />
           </div>
         </div>
-        <div className="flex flex-row justify-center items-center bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] gap-[35px] shadow-sm">
+        <div className="flex flex-row justify-center items-center bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[20px] py-[20px] gap-[20px] shadow-sm">
           <div>
             <p className="text-gray-400">Troubled Machine</p>
-            <p className="font-medium text-[34px]">
-              700
-            </p>
+            <p className="font-medium text-[34px]">700</p>
           </div>
           <div className="bg-[#FF6F6F]/30 rounded-[10px] h-[69px] w-[69px] flex justify-center items-center">
             <MachineErrorIcon />
           </div>
         </div>
-        <div className="flex flex-row justify-center items-center bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] gap-[35px] shadow-sm">
+        <div className="flex flex-row justify-center items-center bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[20px] py-[20px] gap-[20px] shadow-sm">
           <div>
             <p className="text-gray-400">Prediction Accuracy</p>
-            <p className="font-medium text-[34px]">
-              87%
-            </p>
+            <p className="font-medium text-[34px]">87%</p>
           </div>
           <div className="bg-[#FEC53D]/30 rounded-[10px] h-[69px] w-[69px] flex justify-center items-center">
             <AccuracyPredictionIcon />
           </div>
         </div>
       </div>
-      <div className="flex flex-row mt-[40px] gap-10">
-        <div className="bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] shadow-sm w-1/3 h-[429px]">
+      <div className="flex flex-col md:flex-row mt-[40px] gap-10">
+        <div className="bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] shadow-sm w-full md:w-1/3 h-[429px]">
           <p className="font-medium text-[20px]">Fail Type Distribution</p>
           <p>Pie Chart</p>
         </div>
-        <div className="bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] w-2/3 shadow-sm">
+        <div className="bg-white dark:bg-[#081028] border-gray-200 border-[1px] rounded-[15px] px-[24px] py-[29px] w-full md:w-2/3 shadow-sm">
           <p className="font-medium text-[20px]">Temperature Graph</p>
           <p>Line Chart</p>
         </div>
