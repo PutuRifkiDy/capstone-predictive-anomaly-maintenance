c9c65f62f15deb299703c849a7f49f4a2b5127bd
PutuRifkiDy <puturifki56@gmail.com>
2025-12-06 10:24:02 +0800

    updated: styling in chabot content

--- frontend/src/pages/app/chatbot/ChatbotContent.jsx
+++ frontend/src/pages/app/chatbot/ChatbotContent.jsx
@@ -47,101 +47,104 @@ export default function ChatbotContent({ authedUser }) {
   };
 
   return (
-      <div className="relative">
-        <div className="mb-24">
-          {chatHistory.length == 0 ? (
-            <div className="flex flex-col items-center justify-center p-36 gap-5">
-              <img
-                src="/empty-chatbot-icon.png"
-                className="w-24 h-24"
-                alt="empty chatbot"
-              />
-              <p className="font-medium text-[32px] tracking-[-0.11px] text-[#646B72] dark:text-white">
-                Predictive Maintenance Copilot
-              </p>
-            </div>
-          ) : (
-            <>
-              {chatHistory.map((message, index) => (
-                <div key={index} className="flex flex-col mb-1">
+    <div className="relative">
+      <div className="mb-24">
+        {chatHistory.length == 0 ? (
+          <div className="flex flex-col items-center justify-center p-36 gap-5">
+            <img
+              src="/empty-chatbot-icon.png"
+              className="w-24 h-24"
+              alt="empty chatbot"
+            />
+            <p className="font-medium text-[32px] tracking-[-0.11px] text-[#646B72] dark:text-white">
+              Predictive Maintenance Copilot
+            </p>
+          </div>
+        ) : (
+          <>
+            {chatHistory.map((message, index) => (
+              <div key={index} className="flex flex-col mb-1">
+                <div
+                  className={`flex items-center gap-4 ${
+                    message.sender == "agent" ? "justify-start" : "justify-end"
+                  }`}
+                >
                   <div
-                    className={`flex items-center gap-4 ${
+                    className={`flex items-center justify-center w-12 h-12 p-4 rounded-full ${
                       message.sender == "agent"
-                        ? "justify-start"
-                        : "justify-end"
-                    }`}
+                        ? "bg-[#515DEF]/30 border-[1px] border-[#515DEF]/10"
+                        : "bg-[#515DEF]"
+                    } text-white`}
                   >
+                    {message.sender == "agent" ? (
+                      <CpuChipIcon className="w-8 h-8 shrink-0 text-[#515DEF]" />
+                    ) : (
+                      authedUser.name.substring(0, 2).toUpperCase()
+                    )}
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <p className="font-medium text-[#1E293B] dark:text-white">
+                      {message.sender == "agent"
+                        ? "Predicta Chatbot"
+                        : message.sender}
+                    </p>
                     <div
-                      className={`flex items-center justify-center w-12 h-12 p-4 rounded-full ${
+                      className={`p-3 ${
                         message.sender == "agent"
-                          ? "bg-[#515DEF]/30 border-[1px] border-[#515DEF]/10"
-                          : "bg-[#515DEF]"
-                      } text-white`}
+                          ? "bg-gray-50 border-[1px] border-gray-200 dark:bg-[#515DEF]/20 dark:border-[#515DEF]/20 relative rounded-r-lg rounded-br-lg"
+                          : "bg-[#515DEF] border-[1px] border-[#515DEF]/30 rounded-lg"
+                      }`}
                     >
-                      {message.sender == "agent" ? (
-                        <CpuChipIcon className="w-8 h-8 shrink-0 text-[#515DEF]" />
-                      ) : (
-                        authedUser.name.substring(0, 2).toUpperCase()
+                      {message.sender == "agent" && (
+                        <div className="absolute -left-1 right-0 h-full w-[5px] bg-[#515DEF] inset-y-0 rounded-tl-md rounded-bl-lg"></div>
                       )}
-                    </div>
-                    <div className="flex flex-col gap-1">
-                      <p className="font-medium text-[#1E293B] dark:text-white">
-                        {message.sender == "agent"
-                          ? "Predicta Chatbot"
-                          : message.sender}
-                      </p>
-                      <div
-                        className={`p-3 rounded-lg ${
+                      <p
+                        className={`${
                           message.sender == "agent"
-                            ? "bg-gray-50 border-[1px] border-gray-200"
-                            : "bg-[#515DEF] border-[1px] border-[#515DEF]/30"
+                            ? "text-gray-800 dark:text-white"
+                            : "text-white"
                         }`}
                       >
-                        <p
-                          className={`${
-                            message.sender == "agent"
-                              ? "text-gray-800"
-                              : "text-white"
-                          }`}
-                        >
-                          {message.text}
-                        </p>
-                      </div>
-                      {message.sender == "agent" && (
-                        <div className="flex items-center gap-2 mt-2">
-                          <TrashIcon className="w-6 h-6 text-red-500" />
-                          <DocumentDuplicateIcon className="w-6 h-6 text-gray-500" />
-                        </div>
-                      )}
+                        {message.text}
+                      </p>
                     </div>
+                    {message.sender == "agent" && (
+                      <div className="flex items-center gap-2 mt-2">
+                        <TrashIcon className="w-6 h-6 text-red-500" />
+                        <DocumentDuplicateIcon className="w-6 h-6 text-gray-500" />
+                      </div>
+                    )}
                   </div>
                 </div>
-              ))}
-            </>
-          )}
-        </div>
-        <form
-          onSubmit={handleSendMessage}
-          className={`px-4 sm:px-6 lg:px-8 fixed bottom-0 pb-8 z-50 right-0 bg-white ${context == true ? "lg:left-72 left-0 " : "left-24"}`}
-        >
-          <div className="relative flex gap-3">
-            <Input
-              type="text"
-              className="focus-visible:ring-0 py-6 rounded-full pl-5"
-              value={inputMessage}
-              onChange={onInputMessageChange}
-              placeholder="Checking your machine here..."
-            />
-            <Button
-              variant="none"
-              type="submit"
-              className="bg-[#515DEF] text-white flex gap-2 items-center absolute right-2 top-1.5 rounded-full outline-none "
-            >
-              Send
-              <PaperAirplaneIcon className="w-5 h-5 text-white transform rotate-30" />
-            </Button>
-          </div>
-        </form>
+              </div>
+            ))}
+          </>
+        )}
       </div>
+      <form
+        onSubmit={handleSendMessage}
+        className={`px-4 sm:px-6 lg:px-8 fixed bottom-0 pb-8 z-50 right-0 bg-white dark:bg-[#081028] ${
+          context == true ? "lg:left-72 left-0 " : "left-24"
+        }`}
+      >
+        <div className="relative flex gap-3">
+          <Input
+            type="text"
+            className="focus-visible:ring-0 py-6 rounded-full pl-5"
+            value={inputMessage}
+            onChange={onInputMessageChange}
+            placeholder="Checking your machine here..."
+          />
+          <Button
+            variant="none"
+            type="submit"
+            className="bg-[#515DEF] text-white flex gap-2 items-center absolute right-2 top-1.5 rounded-full outline-none "
+          >
+            Send
+            <PaperAirplaneIcon className="w-5 h-5 text-white transform rotate-30" />
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 }
