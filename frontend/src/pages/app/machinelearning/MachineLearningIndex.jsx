c37a423fc7a256419e2ccb588f87cea419944184
Candra Wikananta <candrawikananta@gmail.com>
2025-12-10 22:56:07 +0800

    add: machine learning page

---
+++ frontend/src/pages/app/machinelearning/MachineLearningIndex.jsx
@@ -0,0 +1,101 @@
+import AppLayout from "@/components/layouts/AppLayout";
+import { DocumentArrowUpIcon, DocumentIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useRef, useState } from "react";
+
+export default function MachineLearningIndex({ authedUser, onLogout }) {
+  const [file, setFile] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const inputRef = useRef(null);
+  console.log(file);
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+
+    const droppedFile = e.dataTransfer.files[0];
+    if (droppedFile) {
+      setFile(droppedFile);
+    }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleInputChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  const handleDeleteFile = () => {
+    setFile(null);
+  };
+
+  return (
+    <AppLayout authedUser={authedUser} onLogout={onLogout}>
+      <div>
+        <p className="font-medium text-[32px] tracking-[-0.11px] text-[#000000] dark:text-white">
+          Upload File
+        </p>
+        <form className="flex bg-white border-[1px] border-gray-200 rounded-[15px] w-full h-[533px] py-[65px] px-[47px] gap-10 mt-10 shadow-sm">
+          <div
+            className={`border-2 border-dashed rounded-[8px] w-1/2 flex flex-col items-center justify-center text-center p-10 transition
+              ${isDragging ? "border-[#515DEF] bg-blue-50" : "border-gray-300"}`}
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onClick={() => inputRef.current.click()}
+          >
+            <label
+              htmlFor=""
+              className="w-full h-full flex flex-col items-center justify-center cursor-pointer gap-1"
+            >
+              <DocumentArrowUpIcon className="w-[80px] h-[80px] text-gray-500" />
+              <p className="text-gray-500">
+                Drag & Drop file here or{" "}
+                <span className="text-blue-500 underline">Browse File</span>
+              </p>
+              <p className="text-gray-400">
+                CSV file only
+              </p>
+              <input
+                type="file"
+                className="hidden"
+                onChange={handleInputChange}
+                ref={inputRef}
+              />
+            </label>
+          </div>
+          <div className="flex flex-col w-1/2 justify-between">
+            <div className="flex flex-col gap-[29px]">
+              <p className="font-medium text-xl">Uploaded file</p>
+              {file ? 
+                <div className="border-[1px] rounded-[8px] flex  items-center py-[14px] px-[15px] justify-between">
+                  <div className="flex flex-row gap-5 items-center">
+                    <img src="/csv_icon.png" alt="csv icon" className="w-[38px] h-[38px]"/>
+                    <div className="flex flex-col">
+                      <p>
+                        {file.name}
+                      </p>
+                      <p className="text-gray-500">
+                        {(file.size / 1000000).toFixed(2)} MB
+                      </p>
+                    </div>
+                  </div>
+                  <XMarkIcon className="w-[24px] h-[24px] text-gray-500 cursor-pointer" 
+                  onClick={handleDeleteFile}/>
+                </div> 
+                : "No file selected"}
+            </div>
+            <button className="bg-[#515DEF] text-white w-full rounded-[8px] py-[10px]">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </AppLayout>
+  );
+}
