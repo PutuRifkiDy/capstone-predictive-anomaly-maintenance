0fabfe3b319acaa03548c48a7869f32f9b1e84da
PutuRifkiDy <puturifki56@gmail.com>
2025-12-13 02:08:04 +0800

    added: insert predicta chatbot in users migrations

--- backend/migrations/1764173903194_create-table-users.js
+++ backend/migrations/1764173903194_create-table-users.js
@@ -17,7 +17,8 @@ exports.up = (pgm) => {
     INSERT INTO users(name, email, phone_number, password, role, created_at, updated_at) VALUES 
     ('Admin Putu Rifki', 'puturifki56@gmail.com', '0881038194017', '$2b$10$kujFrozzV8b48pYBjMOZUem7mGQu4E46OsxdKpxzff5RzahYNZ7S2', 'admin', '25-12-04 22:32:01.01072+08', '25-12-04 22:32:01.01072+08'),
     ('Admin Candra Wikananta', 'candrawikananta@gmail.com', '0881038194017', '$2b$10$kujFrozzV8b48pYBjMOZUem7mGQu4E46OsxdKpxzff5RzahYNZ7S2', 'admin', '25-12-04 22:32:01.01072+08', '25-12-04 22:32:01.01072+08'),
-    ('Admin Tri Darma', 'tridarma@gmail.com', '0881038194017', '$2b$10$kujFrozzV8b48pYBjMOZUem7mGQu4E46OsxdKpxzff5RzahYNZ7S2', 'admin', '25-12-04 22:32:01.01072+08', '25-12-04 22:32:01.01072+08');
+    ('Admin Tri Darma', 'tridarma@gmail.com', '0881038194017', '$2b$10$kujFrozzV8b48pYBjMOZUem7mGQu4E46OsxdKpxzff5RzahYNZ7S2', 'admin', '25-12-04 22:32:01.01072+08', '25-12-04 22:32:01.01072+08'),
+    ('Predicta Chatbot', 'predictachatbot@gmail.com', '0881038194017', '$2b$10$kujFrozzV8b48pYBjMOZUem7mGQu4E46OsxdKpxzff5RzahYNZ7S2', 'admin', '25-12-04 22:32:01.01072+08', '25-12-04 22:32:01.01072+08');
   `);
 };
 
