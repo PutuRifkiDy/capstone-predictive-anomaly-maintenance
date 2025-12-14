c73f9f0cf27bd47e4fd1ffa92127dfb872e23d69
PutuRifkiDy <puturifki56@gmail.com>
2025-12-14 15:05:36 +0800

    update: uncomment local configuration database pool

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,16 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false }
+// });
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
 pool.on('connect', () => {
