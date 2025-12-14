036f5d656c4059f498b75d1ca7f8a12f3057807b
PutuRifkiDy <puturifki56@gmail.com>
2025-12-14 16:35:09 +0800

    updated: path ML_BASE_URL in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://backend-prevo.vercel.app";
-const ML_BASE_URL = "https://tridarma6-ml-prevo.hf.space/";
+const ML_BASE_URL = "https://tridarma6-ml-prevo.hf.space";
 
 function getAccessToken() {
   return localStorage.getItem("accessToken");
