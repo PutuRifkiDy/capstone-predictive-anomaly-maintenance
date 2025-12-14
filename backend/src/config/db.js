a748010fe5ea5ae83fc454f130fdd9c26cfd6d52
tridarma6 <triidarma06@gmail.com>
2025-12-14 16:19:18 +0800

    add cors

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,17 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: false }
-// });
-
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
+
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
 
 pool.on('connect', () => {
   console.log('Connected to the database');
